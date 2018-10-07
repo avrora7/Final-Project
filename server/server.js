af5b9bf1c868966d533d5e3aa905be92dde2c36e
avrora7 <avrora7@yahoo.com>
2018-10-07 15:48:46 -0700

    added data layer

--- server/server.js
+++ server/server.js
@@ -34,6 +34,92 @@ sequelize
     console.error('DB connection Failed:', err);
   });
 
+const ProvidedService = sequelize.define('ProvidedService', {
+    id: { type: Sequelize.INTEGER, primaryKey: true },
+    name: Sequelize.STRING,
+    definition: Sequelize.TEXT
+});
+
+const ProvidedSpecialization = sequelize.define('ProvidedSpecialization', {
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    name:Sequelize.STRING,
+    providedServiceId:
+    {
+        type: Sequelize.INTEGER,
+        references: {
+          model: ProvidedService,
+          key: 'id',
+        }
+    }
+});
+
+const User = sequelize.define('user', {
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    company: Sequelize.STRING,
+    email:Sequelize.STRING,
+    password:Sequelize.STRING,
+    isVendor:Sequelize.BOOLEAN,
+    address:Sequelize.STRING,
+    zip:Sequelize.STRING,
+    state:Sequelize.STRING,
+    country:Sequelize.STRING,
+    phone:Sequelize.STRING,
+    manager:Sequelize.STRING,
+    intro:Sequelize.TEXT,
+    ProvidedSpecialization:
+    {
+        type: Sequelize.INTEGER,
+        references: {
+          model: ProvidedSpecialization,
+          key: 'id',
+        }
+    }
+});
+
+const Relationship = sequelize.define('relationship', {
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    vendorId:  {
+        type: Sequelize.INTEGER,
+        references: {
+          model: User,
+          key: 'id',
+        }
+    },
+    startupId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: User,
+          key: 'id',
+        }
+    },
+    status:Sequelize.STRING
+});
+
+const Message = sequelize.define('message', {
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    fromId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: User,
+          key: 'id',
+        }
+    },
+    toId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: User,
+          key: 'id',
+        }
+    },
+    relationshipId:  {
+        type: Sequelize.INTEGER,
+        references: {
+          model: Relationship,
+          key: 'id',
+        }
+    },
+    text:Sequelize.STRING
+});
 
 app.get('/', (req, res) => {
 
@@ -47,4 +133,39 @@ app.get('/', (req, res) => {
     res.json(movies);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+sequelize.sync({force: true}).then(function() {
+    
+    let services = [
+        { id:1, name: 'Venture Capitalist', definition: 'VCs are the people who give money' },
+        { id:2, name: 'Mentor', definition: 'Mentors are the people who tech' },
+        { id:3, name: 'Lawyer', definition: 'Lawyers are the people who take your money' },
+        { id:4, name: 'Incubators & Accelerators', definition: 'Incubators help you gain experience' }
+    ];
+
+    let specializations = [
+        {name: 'Angel', providedServiceId: 1},
+        {name: 'Seed', providedServiceId: 1},
+        {name: 'Mid', providedServiceId: 1},
+        {name: 'Late', providedServiceId: 1},
+
+        {name: 'Marketing', providedServiceId: 2},
+        {name: 'Sales', providedServiceId: 2},
+        {name: 'Human Resources', providedServiceId: 2},
+
+        {name: 'Corporate', providedServiceId: 3},
+        {name: 'Immigration', providedServiceId: 3},
+        {name: 'Intellectual Property', providedServiceId: 3},
+
+        {name: 'Incubators', providedServiceId: 4},
+        {name: 'Accelerators', providedServiceId: 4}
+    ];
+
+    ProvidedService.bulkCreate(
+        services
+    ).then(() => {
+        return ProvidedSpecialization.bulkCreate(specializations)
+    }).then(() => {
+        app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    });
+})
+
